159bc235a799e9eb66b06d8f6267e7ef4a103f81
Manraj Singh <manrajsinghgrover@gmail.com>
2015-10-14 00:34:06 +0530

    Added buttons and toast message

--- Lesson2/app/src/main/java/com/manrajsingh/example/lesson2/MainActivity.java
+++ Lesson2/app/src/main/java/com/manrajsingh/example/lesson2/MainActivity.java
@@ -5,14 +5,25 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import static java.lang.String.*;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button answerYes,answerNo;
+    private EditText userName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        answerYes = (Button) findViewById(R.id.answer_yes_button);
+        answerNo = (Button) findViewById(R.id.answer_no_button);
+        userName = (EditText) findViewById(R.id.users_name);
     }
 
     @Override
@@ -35,5 +46,17 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onYesButtonClick(View view) {
+        String usersName = valueOf(userName.getText());
+        String yourYesResponse = "That is great "+ usersName;
+        Toast.makeText(this,yourYesResponse,Toast.LENGTH_LONG).show();
+    }
+
+    public void onNoButtonClick(View view) {
+        String usersName = valueOf(userName.getText());
+        String yourNoResponse = "Too Bad "+ usersName;
+        Toast.makeText(this,yourNoResponse,Toast.LENGTH_LONG).show();
     }
 }
