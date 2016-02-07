79d82f1d9a44571fa1e0170bf05c05e411252f8a
Manraj Singh <manrajsinghgrover@gmail.com>
2016-02-07 17:09:36 +0530

    Intent extracted

--- MyFirstApp/app/src/main/java/in/manrajsingh/myfirstapp/DisplayMessageActivity.java
+++ MyFirstApp/app/src/main/java/in/manrajsingh/myfirstapp/DisplayMessageActivity.java
@@ -1,11 +1,14 @@
 package in.manrajsingh.myfirstapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -15,7 +18,13 @@ public class DisplayMessageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_display_message);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.content);
+        layout.addView(textView);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
