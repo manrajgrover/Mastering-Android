4a1875a4d6d88efad32f5bfe2dc7e843ec9bca29
Manraj Singh <manrajsinghgrover@gmail.com>
2016-02-07 17:04:36 +0530

    Second activity added

--- MyFirstApp/app/src/main/java/in/manrajsingh/myfirstapp/MyActivity.java
+++ MyFirstApp/app/src/main/java/in/manrajsingh/myfirstapp/MyActivity.java
@@ -1,5 +1,6 @@
 package in.manrajsingh.myfirstapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MyActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "in.manrajsingh.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +30,13 @@ public class MyActivity extends AppCompatActivity {
             }
         });
     }
-
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
