436be9f84c523722d98fedd7f2b8d7515908e71a
Manraj Singh <manrajsinghgrover@gmail.com>
2016-02-10 12:33:16 +0530

    Fragments

---
+++ Fragments/app/src/main/java/in/manrajsingh/fragments/FragmentLandscape.java
@@ -0,0 +1,19 @@
+package in.manrajsingh.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by manraj singh on 2/10/2016.
+ */
+public class FragmentLandscape extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.landscape_fragment,container,false);
+    }
+}
